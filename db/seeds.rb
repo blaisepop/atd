a93376e7fd7e40a9a7e233c7a50951e7f46a8caf
Blaise Popote <blaisepopote@gmail.com>
2020-06-01 15:56:04 +0200

    first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroy all"
+Playlist.destroy_all
+Track.destroy_all
+Dj.destroy_all
+
+puts "Creating Dj's"
+alex = Dj.create!(
+  username: "Dj Alex",
+  email:      "alex@gmail.com",
+  password:   "azerty"
+)
+
+puts "Creating playlist"
+playlist = Playlist.create!(
+  name: "Party rock",
+  room_code: "2020",
+  dj: alex
+)
+
+puts "Creating tracks"
+track = Track.create!(
+  title: "Blinding lights",
+  artist: "The Weeknd",
+  album: "After hours",
+  duration: 200000,
+  votes: 5
+)
