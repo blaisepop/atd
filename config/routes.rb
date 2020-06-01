c1f109d2af0ad0350c7441f2ec1040fd7cf3b54c
Blaise Popote <blaisepopote@gmail.com>
2020-06-01 16:40:50 +0200

    controller playlist

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'playlists/index'
+  get 'playlists/create'
+  get 'playlists/update'
+  get 'playlists/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :playlists do
---
+++ test/controllers/playlists_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PlaylistsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get playlists_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get playlists_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get playlists_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get playlists_destroy_url
+    assert_response :success
+  end
+
+end
