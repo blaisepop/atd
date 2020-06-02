ac18dae39dd03a08057db290e21c79d0cb32f8e1
Blaise Popote <blaisepopote@gmail.com>
2020-06-02 15:55:21 +0200

    destroy button working

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'playlists/index'
-  get 'playlists/create'
-  get 'playlists/update'
-  get 'playlists/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :playlists do
     resources :tracks, only: [:destroy, :new, :create, :edit, :update, :index]
   end
-
 end
