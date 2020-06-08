89b0b0b264a9543e40d4e065ddd4c98e37254b29
Helene van Berchem <h.v.b@hotmail.com>
2020-06-08 16:01:41 +0200

    made changes on front and added date to playlists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_154005) do
+ActiveRecord::Schema.define(version: 2020_06_08_123045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_154005) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "submission", default: true
     t.boolean "visible", default: true
+    t.date "date"
     t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
