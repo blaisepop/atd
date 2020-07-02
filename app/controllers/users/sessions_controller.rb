7b2ced289c77bec14daafc38427910e83fa619ff
Blaise Popote <blaisepopote@gmail.com>
2020-07-02 22:22:00 +0200

    added dropdown and fix roomcode in url

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,7 +13,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    @playlist = Playlist.find(current_user.temp_playlist_id)
+    @playlist = Playlist.friendly.find(current_user.temp_playlist_id)
     @spotify_playlist = @spotify_user.create_playlist!(@playlist.name)
     @playlist_uri = @spotify_playlist.uri
     # Now you can access user's private data, create playlists and much more
@@ -51,7 +51,7 @@ class Users::SessionsController < Devise::SessionsController
     # Get user's top played artists and tracks
     # spotify_user.top_artists #=> (Artist array)
     # spotify_user.top_tracks(time_range: 'short_term') #=> (Track array)
-    redirect_to playlist_path(current_user.temp_playlist_id)
+    redirect_to playlist_path(@playlist.room_code)
   # private
 
   # def sessions_params
