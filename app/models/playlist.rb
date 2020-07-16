2c6bf96b69a0315e2d9a4876adb0f012ca6117f6
Blaise <47299725+blaisepop@users.noreply.github.com>
2020-07-16 11:32:11 +0200

    Update playlist.rb

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,7 +3,7 @@ class Playlist < ApplicationRecord
   has_many :playlist_tracks, dependent: :destroy
   has_many :tracks, through: :playlist_tracks
   has_many :votes, dependent: :destroy
-  validates :name, :date, presence: true
+  validates :name, :date, :room_code, presence: true
   extend FriendlyId
   friendly_id :room_code, use: :slugged
 
